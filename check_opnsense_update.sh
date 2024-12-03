7b3777292b340b07f86739b21ae029cd24914417
f45tb00t <73059676+f45tb00t@users.noreply.github.com>
2024-12-03 22:13:48 +0100

    Update check_opnsense_update.sh
    ....  a != b....

--- check_opnsense_update.sh
+++ check_opnsense_update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# Aktuelle installierte Version abrufen
-CURRENT_VERSION=$(opnsense-version)
+# Aktuelle installierte Version abrufen und nur die Versionsnummer extrahieren
+CURRENT_VERSION=$(opnsense-version | awk '{print $2}')
 
 # Verfügbare Version aus dem Repository abrufen
 AVAILABLE_VERSION=$(pkg rquery '%v' opnsense)
@@ -13,6 +13,6 @@ if [ "$CURRENT_VERSION" = "$AVAILABLE_VERSION" ]; then
     exit 0
 else
     # Update verfügbar
-    echo "UPDATE_AVAILABLE: Current version: $CURRENT_VERSION, Available version: OPNsense $AVAILABLE_VERSION (amd64)"
+    echo "UPDATE_AVAILABLE: Current version: OPNsense $CURRENT_VERSION, Available version: OPNsense $AVAILABLE_VERSION"
     exit 1
 fi
