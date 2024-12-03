a300c8c7ab18f9d0f7dd0fc8ee75a18f88dc77e0
f45tb00t <73059676+f45tb00t@users.noreply.github.com>
2024-12-03 21:27:23 +0100

    Update check_opnsense_update.sh
    as a new version came out I could test. opnsense-update -c  does not deliver the ouput I expected. Fixed now

--- check_opnsense_update.sh
+++ check_opnsense_update.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+# Aktuelle installierte Version abrufen
 CURRENT_VERSION=$(opnsense-version)
-AVAILABLE_VERSION=$(opnsense-update -c)
 
-if [ -z "$AVAILABLE_VERSION" ]; then
+# Verfügbare Version aus dem Repository abrufen
+AVAILABLE_VERSION=$(pkg rquery '%v' opnsense)
+
+if [ "$CURRENT_VERSION" = "$AVAILABLE_VERSION" ]; then
+    # Keine neue Version verfügbar
     echo "NO_UPDATE: Current version: $CURRENT_VERSION"
     exit 0
 else
-    echo "UPDATE_AVAILABLE: Current version: $CURRENT_VERSION, Available version: $AVAILABLE_VERSION"
+    # Update verfügbar
+    echo "UPDATE_AVAILABLE: Current version: $CURRENT_VERSION, Available version: OPNsense $AVAILABLE_VERSION (amd64)"
     exit 1
 fi
